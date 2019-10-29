a99038abbadfa3284a3fa6b890c214ea93dfe52c
Usuario <sergio_kay18@hotmail.com>
2019-10-29 10:58:17 +0100

    29/10/19

--- Python/Unidad tres/Unidad 3,7.py
+++ Python/Unidad tres/Unidad 3,7.py
@@ -1,13 +1,12 @@
 #Introducir	por	teclado	un	numero	y	mostrar	en	pantalla
 #la	serie:	1,	-2,	3,	-4,.........,	n o	–n
 
-i=int(input("Introduce un numero:"))
+i=int(input("Introduce un numero:"))        #Introducimos un numero
 
-for i in range(1,i+1):
-    
-    if i%2 == 0:
+for i in range(1,i+1):                      # Un rango de 1 hasta el valor que yo le de                                   
+    if i%2 == 0:                            # si el numoro es par lo ponemos negativo
         i=-i
-    print(i,sep=" ",end=",")
+    print(i,sep=" ",end=",")                # mostramos el numero de forma horizontal
     i+=1
 
 
--- Python/Unidad tres/Unidad 3,9.py
+++ Python/Unidad tres/Unidad 3,9.py
@@ -1,5 +1,17 @@
 #Da dos	números	enteros	(un	dividendo	y	un	divisor	distinto	de	0)	obtener	su	cociente
 #y	el	resto	mediante	restas	sucesivas.
 
-dividendo=int(input("Introduzca un dividendo:"))
-divisor=int(input("Introduzca un divisor:"))
+
+cociente=0
+dividendo=int(input("Introduzca un dividendo:")) #31
+divisor=int(input("Introduzca un divisor:"))     #5
+
+while dividendo != 0:           #Mientras el dividendo sea diferente a 0 se ejecuta
+    if (dividendo >= divisor):  #Si el dividendo es mayor al divisor se hace la cuenta
+        cociente+=1             #El cociente cuenta las vueltas que hace para ir restando el resto
+        dividendo=dividendo-divisor     #El dividendo es el resto que s eva restando una y otra vez
+print("este es el cociente",cociente,"y este el resto",dividendo)   # mostramos el cociente y el dividendo
+
+
+
+ 
